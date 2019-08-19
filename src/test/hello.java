72309da50878c933ca380ae9ab0951382fdb6629
lazyTimes <1097483508@qq.com>
2019-08-19 14:35:20 +0800

    测试提交

---
+++ src/test/hello.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @author zhaoxudong
+ * @title: hello
+ * @projectName structAlgorithms
+ * @description: TODO
+ * @date 2019/8/19 14:34
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.err.println("测试提交");
+    }
+}
