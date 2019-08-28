193efe4a70aafef4184466d110ab324631496bb0
lazyTimes <1097483508@qq.com>
2019-08-28 19:22:24 +0800

    链表的相关算法

--- src/queue/SingelQueue.java
+++ src/queue/SingelQueue.java
@@ -1,6 +1,7 @@
 package queue;
 
 import java.util.LinkedList;
+import java.util.Stack;
 
 /**
  * 单链表实现
@@ -23,16 +24,98 @@ public class SingelQueue {
         singelLinkedList.addByOrder(heroNode3);
         singelLinkedList.addByOrder(heroNode4);
         singelLinkedList.delByIndex(4);
+        singelLinkedList.reverse();
         singelLinkedList.list();
-
-        LinkedList linkedList = new LinkedList();
+        System.err.println("count =  " + singelLinkedList.count());
+        System.err.println("倒数" + singelLinkedList.getLastIndexHeroNode(4));
     }
 }
 
 
 class SingelLinkedList {
+    /**
+     * 头节点，用来存储首节点的位置
+     * 不存储数据
+     */
     private HeroNode head = new HeroNode(0, "", "");
 
+
+    /**
+     * 合并两个链表，合并之后依然有序
+     * 1. 计算两个链表的个数
+     * 2. 循环总数，判断两个节点的当前节点更大，
+     *
+     * @param list1
+     * @param list2
+     * @return
+     */
+    public static HeroNode mergeList(HeroNode list1, HeroNode list2) {
+        HeroNode result = new HeroNode(0, "", "");
+
+        return result;
+    }
+
+    public void reversePrint() {
+        if (head == null || head.getNext() == null || head.getNext().getNext() == null) {
+            System.err.println("无需反转打印");
+            return;
+        }
+
+        Stack<HeroNode> stack = new Stack<>();
+
+    }
+
+    /**
+     * 反转节点
+     * 1. 定义一个反转节点的head
+     * 2. 遍历原来的链表，使用类似入栈的方式，将每个节点接入到反转节点的指向节点，被接入节点指向原来的节点
+     * 3. 反转节点完成
+     */
+    public void reverse() {
+        if (head == null || head.getNext() == null || head.getNext().getNext() == null) {
+            System.err.println("无需反转");
+            return;
+        }
+        HeroNode reverseHead = new HeroNode(0, "", "");
+        HeroNode temp = head.getNext();
+        HeroNode next = null;
+        while (temp != null) {
+            // 让当前节点接到新链表的下一个节点
+            next = temp.getNext();
+            temp.setNext(reverseHead.getNext());
+            reverseHead.setNext(temp);
+            temp = next;
+        }
+        // 使用反转节点替换原来的节点
+        head.setNext(reverseHead.getNext());
+    }
+
+    /**
+     * 获取倒数第index个节点的值
+     *
+     * @param index
+     * @return
+     */
+    public HeroNode getLastIndexHeroNode(int index) {
+        // 1. 获取链表的总个数
+        int count = count();
+        if (index <= 0 || index > count) {
+            return null;
+        }
+        HeroNode temp = head.getNext();
+        // 2. 根据size - index , 循环获取倒数第 index 节点
+        int i = 0;
+        int size = count - index;
+        while (true) {
+            if (i == size) {
+                break;
+            }
+            i++;
+            temp = temp.getNext();
+        }
+        // 3. 遍历拿到数据，返回结果
+        return temp;
+    }
 
     /**
      * 在插入的使用最新的方式：
@@ -52,6 +135,35 @@ class SingelLinkedList {
             heroNode = heroNode.getNext();
         }
         heroNode.setNext(node);
+
+    }
+
+    public static void addByOrder(HeroNode head, HeroNode add) {
+// 1. 由于头结点不能动，需要使用
+        HeroNode heroNode = head;
+        boolean flag = false;
+        while (true) {
+            if (heroNode.getNext() == null) {
+                break;
+            }
+            // 寻找编号大于被插入节点的节点，因为只能找被插入的节点的上一个节点
+            if (heroNode.getNext().getNo() > add.getNo()) {
+                // 获取原来数据的下一个节点一个节点
+                break;
+            } else if (heroNode.getNext().getNo() == add.getNo()) {
+                //说明希望添加的heroNode的编号已然存在
+                flag = true;
+                break;
+            }
+            heroNode = heroNode.getNext();
+        }
+
+        if (flag) {
+            System.err.println("被插入的节点已经存在");
+        } else {
+            add.setNext(heroNode.getNext());
+            heroNode.setNext(add);
+        }
 
     }
 
@@ -94,9 +206,10 @@ class SingelLinkedList {
 
     /**
      * 被更新的节点，根据编号匹配找到对应节点
+     *
      * @param heroNode
      */
-    public void update(HeroNode heroNode){
+    public void update(HeroNode heroNode) {
         HeroNode temp = head;
         boolean flag = false;
         while (true) {
@@ -106,11 +219,11 @@ class SingelLinkedList {
             }
             temp = temp.getNext();
         }
-        if(flag){
+        if (flag) {
             temp.setNo(heroNode.getNo());
             temp.setName(heroNode.getName());
             temp.setNickName(heroNode.getNickName());
-        }else{
+        } else {
             System.err.println("没有找到对应的节点");
         }
     }
@@ -149,6 +262,24 @@ class SingelLinkedList {
     }
 
     /**
+     * 单链表当中有效节点的个数
+     *
+     * @return 有效节点的个数
+     */
+    public int count() {
+        HeroNode heroNode = head.getNext();
+        int count = 0;
+        while (true) {
+            if (heroNode == null) {
+                break;
+            }
+            count++;
+            heroNode = heroNode.getNext();
+        }
+        return count;
+    }
+
+    /**
      * 删除的第二张方式‘：
      * 根据index索引进行删除
      *
@@ -166,7 +297,7 @@ class SingelLinkedList {
         boolean flag = false;
         int count = 0;
         while (true) {
-            if(index > count){
+            if (index > count) {
                 break;
             }
             if (index == count++) {
